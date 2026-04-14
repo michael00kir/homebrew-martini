0234e52704b24ea19e5300bd51efc37c603ad099
Michael Kir <michael.kir@icloud.com>
2026-04-14 22:29:38 +0100

    Update revision hash for martini formula

--- Formula/martini.rb
+++ Formula/martini.rb
@@ -6,7 +6,7 @@ class Martini < Formula
   # to be defined explicitly or inferred correctly from the tag.
   url "https://github.com/michael00kir/martini.git",
       tag:      "1.0.0",
-      revision: "574c86e88c07e6005d535359a16f849b67919812" # Example hash
+      revision: "1b4f82ee965f949d0d119478b39b15d94ed1bb9f" # Example hash
   
   # Explicitly define the version to prevent 'version (nil)' errors
   version "1.0.0" 
