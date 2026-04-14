6198b9c1cf20339ee624718ea72fe49cd654575a
Michael Kir <michael.kir@icloud.com>
2026-04-14 15:28:59 +0100

    Fix URL format for downloading martini source

--- Formula/martini.rb
+++ Formula/martini.rb
@@ -2,7 +2,7 @@ class Martini < Formula
   desc "Professional Zsh Terminal & CLI orchestrator built in Swift"
   homepage "https://github.com/michael00kir/martini"
   # Use the #{version} variable so the automation can swap it easily
-  url "https://github.com/michael00kir/martini/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/michael00kir/martini/archive/v#{version}.tar.gz"
   version "1.0.1" # The automation will update this line automatically
   sha256 "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
   license "MIT"
