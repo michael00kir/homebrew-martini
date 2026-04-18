378e018808f1ff45770cdbbc01ddbe1943df1cdd
Michael Kir <michael.kir@icloud.com>
2026-04-18 17:41:03 +0100

    Update URL to use releases tag for martini

--- Formula/martini.rb
+++ Formula/martini.rb
@@ -1,7 +1,7 @@
 class Martini < Formula
   desc "Agent with a Twist"
   homepage "https://martini.systems"
-  url "https://github.com/michael00kir/homebrew-martini/releases/download/v1.0.0/martini.zip"
+  url "https://github.com/michael00kir/homebrew-martini/releases/tag/1.0.0/martini.zip"
   sha256 "tb59686df10c02062b6677957237b6bd8a340aca5"
 
   def install
