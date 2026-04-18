50fcd00646b3334c482d0ee754d0493a4adb82a7
Michael Kir <michael.kir@icloud.com>
2026-04-18 17:54:31 +0100

    Update SHA256 checksum for martini.rb
    Updated the SHA256 checksum for the martini formula.

--- Formula/martini.rb
+++ Formula/martini.rb
@@ -3,7 +3,7 @@ class Martini < Formula
   homepage "https://martini.systems"
   # Updated to the direct download URL
   url "https://github.com/michael00kir/homebrew-martini/releases/download/1.0.0/martini.zip"
-  sha256 "b59686df10c02062b6677957237b6bd8a340aca5"
+  sha256 "eff540d042b4b359376ca6776cf3b6744f4e9338ca5bb506187b452e6dab8cba"
   version "1.0.0"
 
   def install
